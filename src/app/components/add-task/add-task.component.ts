9e27d0f4aaacee9eae52c41a4df20436d74cf87e
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-02-28 14:27:20 +0100

    Implemented Add New Task functionality

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
   text!: string;
-  dat!: string;
+  day!: string;
   reminder: boolean = false;
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task!');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
